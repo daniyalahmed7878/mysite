19b4fb248beb95820b7ab417f4f4d4371be809d9
Daniyal Ahmed Sethi <daniyalahmedsethi@gmail.com>
2024-06-13 02:14:22 +0500

    Home Testing

---
+++ home/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+def test(request):
+    t = loader.get_template("test.html")
+    return HttpResponse(t.render())
